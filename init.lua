59567ccbdc7050bbf2c12f30a608b305fbf1701d
jonhorner-twinkl <167476216+jonhorner-twinkl@users.noreply.github.com>
2024-04-18 22:19:31 +0100

    Update init.lua - set nerd font to true

--- init.lua
+++ init.lua
@@ -91,12 +91,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+--  For more options, you can see `:help option-list` 
 
 -- Make line numbers default
 vim.opt.number = true
